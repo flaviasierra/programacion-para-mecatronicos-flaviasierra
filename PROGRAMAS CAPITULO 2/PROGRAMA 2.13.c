1dfeb45114feae62515a42f068a62f81c2d5977c
flaviasierra <157134429+flaviasierra@users.noreply.github.com>
2024-02-02 16:15:55 -0400

    Add files via upload

---
+++ PROGRAMAS CAPITULO 2/PROGRAMA 2.13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/* funcion*/
+void main(void)
+{
+    float X;
+    int Y;
+    printf("Ingrese el valor de Y: ");
+    scanf("%d", &Y);
+if (Y < 0 ││ Y > 50)
+        x = 0;
+    else
+        if (y <= 10)
+        X = 4/ Y - Y;
+    else
+        if(y <= 25)
+        X = pow (Y, 3) - 12;
+    else
+        X = pow( Y, 2) + pow(Y, 3) -18;
+    printf("\n\nY = %d\tx = %8.2f", Y, X);
+    return 0;
+}
